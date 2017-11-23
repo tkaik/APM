f3597d6db0ed9177bf16807e30611e88e24c72a7
Bartosz Wesolowski <bartosz.wesolowski@cognifide.com>
2017-11-23 11:28:42 +0100

    New edge format

--- bundle/src/main/java/com/cognifide/cq/cqsm/graph/data/Edge.java
+++ bundle/src/main/java/com/cognifide/cq/cqsm/graph/data/Edge.java
@@ -1,12 +1,25 @@
 package com.cognifide.cq.cqsm.graph.data;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Edge {
+	@SerializedName("fromNode")
 	private Node from;
+
+	@SerializedName("toNode")
 	private Node to;
+
+	@SerializedName("from")
+	String fromId;
+
+	@SerializedName("to")
+	String toId;
 
 	public Edge(Node from, Node to) {
 		this.from = from;
+		this.fromId = from.getId();
 		this.to = to;
+		this.toId = to.getId();
 	}
 
 	public Node getFrom() {
